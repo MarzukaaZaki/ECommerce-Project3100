58e2179fe45571ebc3cb3450493162515a8f2546
MarzukaaZaki <zakimarzuka@gmail.com>
2021-12-04 21:20:37 +0600

    added pagination

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,9 @@
+from django.core import paginator
 from django.shortcuts import get_object_or_404, render
 from products.models import Product
 from categories.models import Category
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 def homePageView(response, category_slug = None):
@@ -13,15 +15,25 @@ def homePageView(response, category_slug = None):
 
         #return products of the clicked category
         products = Product.objects.filter(category=categories)
+        # pagination of products
+        p = Paginator(products,6)
     else:
         # else display all products
         products = Product.objects.all()
-
+        
+        # pagination of products
+        p = Paginator(products,9)
+    
     # in both cases display all categories    
     categories = Category.objects.all()
+    
+    # set the links to each page number 
+    page_number = response.GET.get('page')
+    paged_products = p.get_page(page_number)
 
     
-    context  ={'categories':categories,'products':products,}
+    context  ={'categories':categories,'products':paged_products,}
+
     return render(response, 'home.html',context)
 
 
